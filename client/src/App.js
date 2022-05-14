6401843d93fdf80fa159babbd5a7947b6ab484c9
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-05-14 13:18:04 -0400

    return h1 and input elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>CRUD App with MERN STACK</h1>
+      </header>
+      <main>
+        <input type="text" />
+        <input type="number" />
+      </main>
+    </div>
+  );
 }
 
 export default App;
